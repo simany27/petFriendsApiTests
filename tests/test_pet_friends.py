40535cb942559e8cc4a8af87ccdb3bdec3477726
simany27 <simany27@gmail.com>
2023-02-12 17:55:44 +0200

    19/7/2

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -4,6 +4,7 @@ import os
 
 pf = PetFriends()
 
+
 def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
     assert status == 200
